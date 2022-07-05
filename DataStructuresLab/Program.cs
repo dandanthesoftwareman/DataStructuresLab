d69ff890cfcc77d5d6dd9b10a078920376ee615c
Dan Schuler <104746354+dandanthesoftwareman@users.noreply.github.com>
2022-07-05 08:39:24 -0400

    first commit

---
+++ DataStructuresLab/Program.cs
@@ -0,0 +1,71 @@
+﻿
+bool runProgram = true;
+string input = "";
+
+while (runProgram)
+{
+    try
+    {
+        Console.WriteLine("Please enter a word you would like reversed");
+        input = Console.ReadLine();
+        if (input.Any(Char.IsDigit))
+        {
+            throw new Exception("Please refrain from using numbers");
+        }
+        else if (input.Any(Char.IsSymbol))
+        {
+            throw new Exception("Please refrain from using symbols");
+        } 
+        else
+        {
+            Console.WriteLine(wordReverse(input));
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+    while(true)
+    {
+        Console.WriteLine("Reverse again? y/n");
+        string choice = Console.ReadLine().ToLower();
+        if (choice == "y")
+        {
+            break;
+        }
+        else if (choice == "n")
+        {
+            Console.WriteLine("Goodbye!");
+            runProgram = false;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Please type y/n");
+        }
+    }
+}
+
+static string wordReverse(string sentence)
+{
+    string reversed = "";
+    Stack<char> wordStack = new Stack<char>();
+
+    foreach (char c in sentence)
+    {
+        wordStack.Push(c);
+    }
+
+    foreach (char c in wordStack)
+    {
+        reversed += c;
+    }
+
+    string result = "";
+    string[] wordsReverseOrder = reversed.Split(' ');
+    for (int i = wordsReverseOrder.Length - 1; i >= 0; i--)
+    {
+        result += wordsReverseOrder[i] + " ";
+    }
+    return result;
+}
